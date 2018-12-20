1441b59ee9558e60349df33dc12b082c7f7d5b20
Ky Nguyen <kynguyen@Kys-MacBook-Pro-3.local>
2018-12-20 09:59:50 +0700

    Initial Commit

---
+++ CaloriesTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CaloriesTracker
+//
+//  Created by Ky Nguyen Coinhako on 12/20/18.
+//  Copyright © 2018 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
