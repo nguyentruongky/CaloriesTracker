ab94b930a3f8238ee397b9c8118f7d35c5b52ae9
Ky Nguyen <kynguyen@Kys-MacBook-Pro-3.local>
2018-12-20 16:11:33 +0700

    ADD: Password Validation

--- CaloriesTracker/common/PasswordValidation.swift
+++ CaloriesTracker/common/PasswordValidation.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct snPasswordValidation {
+struct CTPasswordValidation {
     func checkCharCount(_ password: String) -> Bool {
         let regexCharCount = "^.{8,}$"
         let passwordTest1 = NSPredicate(format: "SELF MATCHES %@", regexCharCount)
